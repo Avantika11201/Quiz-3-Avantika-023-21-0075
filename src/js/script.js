9d7ab705cf6d9c0c08d1bd81ad228f00f432cf21
Avantika11201 <avantikajewani.bscsf21@iba-suk.edu.pk>
2023-10-02 15:04:51 +0500

    sixth commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("myForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the default form submission
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
 
-        // Get the values from the form inputs
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
+    // Get the values from the form inputs
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-        // You can perform further validation or send the data to a server here
-        // For now, let's just log the values to the console
-        console.log("Email: " + email);
-        console.log("Password: " + password);
+    // Print values to the console
+    console.log("Email: " + email);
+    console.log("Password: " + password);
 
-        
-    });
+    // You can also perform further actions or validation here
 });
